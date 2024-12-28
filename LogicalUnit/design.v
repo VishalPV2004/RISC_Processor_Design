35e759bc5c73453f816cfaff589c0e763ec97714
PVividPulse <125368625+VishalPV2004@users.noreply.github.com>
2024-12-28 09:33:05 +0530

    Add files via upload

---
+++ LogicalUnit/design.v
@@ -0,0 +1,89 @@
+`timescale 1ns / 1ps
+ 
+//--------------------------------------> fields of IR
+`define oper_type IR[31:27]
+`define rdst      IR[26:22]
+`define rsrc1     IR[21:17]
+`define imm_mode  IR[16]
+`define rsrc2     IR[15:11]
+`define isrc      IR[15:0]
+ 
+ 
+//--------------------------------------> arithmetic operation
+`define movsgpr        5'b00000
+`define mov            5'b00001
+`define add            5'b00010
+`define sub            5'b00011
+`define mul            5'b00100
+ 
+ 
+ 
+ 
+module top();
+
+ 
+reg [31:0] IR;            ////// instruction register  <--ir[31:27]--><--ir[26:22]--><--ir[21:17]--><--ir[16]--><--ir[15:11]--><--ir[10:0]-->
+                          //////fields                 <---  oper  --><--   rdest --><--   rsrc1 --><--modesel--><--  rsrc2 --><--unused  -->             
+                          //////fields                 <---  oper  --><--   rdest --><--   rsrc1 --><--modesel--><--  immediate_date      -->      
+ 
+reg [15:0] GPR [31:0] ;   ///////general purpose register gpr[0] ....... gpr[31]
+ 
+ 
+ 
+reg [15:0] SGPR ;      ///// msb of multiplication --> special register
+ 
+reg [31:0] mul_res;
+ 
+ 
+ 
+always@(*)
+begin
+case(`oper_type)
+//----------------------------------> Move contents of SGPR to Destination
+`movsgpr: begin
+ 
+   GPR[`rdst] = SGPR;
+   
+end
+ 
+//----------------------------------> Move contents of one reg to another
+`mov : begin
+   if(`imm_mode)
+        GPR[`rdst]  = `isrc;
+   else
+       GPR[`rdst]   = GPR[`rsrc1];
+end
+ 
+//----------------------------------> Add the contents of two regs
+ 
+`add : begin
+      if(`imm_mode)
+        GPR[`rdst]   = GPR[`rsrc1] + `isrc;
+     else
+        GPR[`rdst]   = GPR[`rsrc1] + GPR[`rsrc2];
+end
+ 
+//----------------------------------> Sub the contents of two regs
+ 
+`sub : begin
+      if(`imm_mode)
+        GPR[`rdst]  = GPR[`rsrc1] - `isrc;
+     else
+       GPR[`rdst]   = GPR[`rsrc1] - GPR[`rsrc2];
+end
+ 
+//---------------------------------> Multiply contents of two regs
+ 
+`mul : begin
+      if(`imm_mode)
+        mul_res   = GPR[`rsrc1] * `isrc;
+     else
+        mul_res   = GPR[`rsrc1] * GPR[`rsrc2];
+        
+     GPR[`rdst]   =  mul_res[15:0];
+     SGPR         =  mul_res[31:16];
+end
+ 
+endcase
+end
+endmodule
